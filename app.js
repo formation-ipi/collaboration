e1ff5e023a8613b65cdc26fc6678e0fc5098f6d4
Jordan Martin <jo69270@gmail.com>
2020-10-19 16:12:07 +0200

    Projet base

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const todos = [
+    {
+        title: 'Formation GIT',
+        done: true
+    },
+    {
+        title: 'TP GIT',
+        done: false
+    }
+];
+
+app.use(bodyParser.json());
+
+// Log des requêtes
+app.use((req, res, next) => {
+    console.log(new Date().toISOString(), req.method, req.url);
+    next();
+});
+
+app.route('/todos')
+    // Récupération des todos
+    .get((req, res) => {
+        res.json(todos);
+    })
+    // Création d'un todo
+    .post((req, res) => {
+        todos.push(req.body);
+        // HTTP 201 => Created
+        res.sendStatus(201);
+    });
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
